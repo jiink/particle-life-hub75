3c3aad8752559ccbe97b07a3163b270e090a9837
jiink <jacob.j.carlson03@gmail.com>
2023-04-29 18:47:40 -0700

    Made it so particles render additively
    using ram framebuffer now

--- particle-life-simulation/main.cpp
+++ particle-life-simulation/main.cpp
@@ -1,6 +1,7 @@
 // TODO: Cell optimization https://youtu.be/9IULfQH7E90?t=231
 // Resources:
 // 		https://youtu.be/scvuli-zcRc
+//		https://www.reddit.com/r/raylib/comments/hcglzh/c_reasonable_performance_pixelbypixel_display/g212jbl/
 
 #include <iostream>
 #include <raylib.h>
@@ -9,8 +10,8 @@
 #define CANVAS_WIDTH (64 * 1) // Resolution of what you want to draw to
 #define CANVAS_HEIGHT (32 * 1)
 #define CANVAS_ASPECT_RATIO (CANVAS_WIDTH / CANVAS_HEIGHT)
-#define SCREEN_WIDTH (CANVAS_WIDTH * 4) // How big will it be on your screen?
-#define SCREEN_HEIGHT (CANVAS_HEIGHT * 4)
+#define SCREEN_WIDTH (CANVAS_WIDTH * 6) // How big will it be on your screen?
+#define SCREEN_HEIGHT (CANVAS_HEIGHT * 6)
 
 #define MAX_PARTICLES 250
 #define MAX_COLOR_GROUPS 2
@@ -23,6 +24,11 @@ using namespace std;
 
 static void Initialize(void);
 static void UpdateDrawFrame(void); // Update and draw one frame
+
+// Set and get pixels from here
+Color FrameBuffer[CANVAS_HEIGHT][CANVAS_WIDTH];
+
+// Image canvasImage = { .data = FrameBuffer, .width = CANVAS_WIDTH, .height = CANVAS_HEIGHT, .mipmaps = 1, .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8 };
 
 enum ColorGroup
 {
@@ -54,6 +60,79 @@ const float dt = 0.01;
 const float frictionFactor = pow(0.5, dt / frictionHalfLife);
 const float forceFactor = 5.0;
 
+// If there is an overflow, return 255
+uint8_t AddClamp(uint8_t a, uint8_t b)
+{
+	uint8_t sum = a + b;
+	if (sum < a || sum < b)
+	{
+		return 255;
+	}
+	else
+	{
+		return sum;
+	}
+}
+
+Color ColorAdd(Color a, Color b)
+{
+	return {
+		AddClamp(a.r, b.r),
+		AddClamp(a.g, b.g),
+		AddClamp(a.b, b.b),
+		AddClamp(a.a, b.a)};
+}
+
+void FrameBufferClear(Color color)
+{
+	for (int y = 0; y < CANVAS_HEIGHT; y++)
+	{
+		for (int x = 0; x < CANVAS_WIDTH; x++)
+		{
+			FrameBuffer[y][x] = color;
+		}
+	}
+}
+
+void FrameBufferSetPix(int x, int y, Color color)
+{
+	if (x < 0 || y < 0)
+		return;
+	if (x > CANVAS_WIDTH - 1 || y > CANVAS_HEIGHT - 1)
+		return;
+	FrameBuffer[y][x] = color;
+}
+
+void FrameBufferSetPixV(Vector2 pos, Color color)
+{
+	FrameBufferSetPix(pos.x, pos.y, color);
+}
+
+Color FrameBufferGetPix(int x, int y)
+{
+	return FrameBuffer[y][x];
+}
+
+Color FrameBufferGetPixV(Vector2 pos)
+{
+	return FrameBufferGetPix(pos.x, pos.y);
+}
+
+void FrameBufferAddPix(int x, int y, Color color)
+{
+	if (x < 0 || y < 0)
+		return;
+	if (x > CANVAS_WIDTH - 1 || y > CANVAS_HEIGHT - 1)
+		return;
+	FrameBufferSetPix(x, y, ColorAdd(FrameBufferGetPix(x, y), color));
+}
+
+void FrameBufferAddPixV(Vector2 pos, Color color)
+{
+	FrameBufferAddPix(pos.x, pos.y, color);
+}
+
+
 //----------------------------------------------------------------------------------
 // Main entry point
 //----------------------------------------------------------------------------------
@@ -71,10 +150,17 @@ int main(void)
 
 	while (!WindowShouldClose()) // Detect window close button or ESC key
 	{
-
 		// Draw on the canvas
 		BeginTextureMode(renderTexture);
 		UpdateDrawFrame();
+		// Transfer colors from array to rendertexture
+		for (int y = 0; y < CANVAS_HEIGHT; y++)
+		{
+			for (int x = 0; x < CANVAS_WIDTH; x++)
+			{
+				DrawPixel(x, y, FrameBufferGetPix(x, y));
+			}
+		}
 		EndTextureMode();
 
 		// Draw the canvas to the actual screen (scaling up)
@@ -199,12 +285,15 @@ void DrawPoint(Vector2 position, Color color)
 	Color colorBottomRight = MultiplyColor(color, areaBottomRight);
 
 	// Set pixels
-	DrawPixelV(pixelCornerTopLeft, colorTopLeft);
-	DrawPixelV(pixelCornerTopRight, colorTopRight);
-	DrawPixelV(pixelCornerBottomLeft, colorBottomLeft);
-	DrawPixelV(pixelCornerBottomRight, colorBottomRight);
+	// DrawPixelV(pixelCornerTopLeft, colorTopLeft);
+	// DrawPixelV(pixelCornerTopRight, colorTopRight);
+	// DrawPixelV(pixelCornerBottomLeft, colorBottomLeft);
+	// DrawPixelV(pixelCornerBottomRight, colorBottomRight);
+	FrameBufferAddPixV(pixelCornerTopLeft, colorTopLeft);
+	FrameBufferAddPixV(pixelCornerTopRight, colorTopRight);
+	FrameBufferAddPixV(pixelCornerBottomLeft, colorBottomLeft);
+	FrameBufferAddPixV(pixelCornerBottomRight, colorBottomRight);
 }
-
 
 float AttractionForceMag(float distance, float attractionFactor)
 {
@@ -226,9 +315,12 @@ float AttractionForceMag(float distance, float attractionFactor)
 	}
 }
 
-void randomizeAttractionFactorMatrix() {
-	for(int i = 0; i < MAX_COLOR_GROUPS; i++){
-		for(int j = 0; j < MAX_COLOR_GROUPS; j++){
+void randomizeAttractionFactorMatrix()
+{
+	for (int i = 0; i < MAX_COLOR_GROUPS; i++)
+	{
+		for (int j = 0; j < MAX_COLOR_GROUPS; j++)
+		{
 			attractionFactorMatrix[j][i] = RandFloat(-1.0, 1.0);
 		}
 	}
@@ -241,15 +333,19 @@ static void Initialize()
 	{
 		particles[i].position = {RandFloat(0, CANVAS_ASPECT_RATIO), RandFloat(0, 1)};
 		// particles[i].velocity = { RandFloat(-50, 50), RandFloat(-50, 50) };
-		//particles[i].velocity = {RandFloat(-1, 1), RandFloat(-1, 1)};
-		 particles[i].velocity = {0.0, 0.0};
+		// particles[i].velocity = {RandFloat(-1, 1), RandFloat(-1, 1)};
+		particles[i].velocity = {0.0, 0.0};
 
 		particles[i].colorGroup = (ColorGroup)RandByte(GROUP_RED, MAX_COLOR_GROUPS - 1);
 
 		// particles[i].colorDraw = ColorGroupColors[particles[i].colorGroup];
 	}
 
-	randomizeAttractionFactorMatrix();
+	//randomizeAttractionFactorMatrix();
+	attractionFactorMatrix[0][0] = 1.0;
+	attractionFactorMatrix[0][1] = -1.0;
+	attractionFactorMatrix[1][0] = 0.4;
+	attractionFactorMatrix[1][1] = 0.0;
 }
 
 static void UpdateDrawFrame()
@@ -260,7 +356,7 @@ static void UpdateDrawFrame()
 	float deltaTime = (currentMillis - prevMillis) / 1000.0f;
 	prevMillis = currentMillis;
 
-	ClearBackground(BLACK);
+	FrameBufferClear({0, 0, 0, 255});
 
 	// Update each particle
 	for (int i = 0; i < MAX_PARTICLES; i++)
@@ -290,7 +386,6 @@ static void UpdateDrawFrame()
 		particles[i].velocity = Vector2Scale(particles[i].velocity, frictionFactor);
 		particles[i].velocity = Vector2Add(particles[i].velocity, Vector2Scale(totalForce, deltaTime));
 
-
 		// Update the particle's position based on its velocity
 		particles[i].position.x += particles[i].velocity.x * deltaTime;
 		particles[i].position.y += particles[i].velocity.y * deltaTime;
@@ -312,5 +407,6 @@ static void UpdateDrawFrame()
 		// Scale from world space to screen space
 		Vector2 posOnScreen = {particles[i].position.x * CANVAS_WIDTH / (CANVAS_ASPECT_RATIO), particles[i].position.y * CANVAS_HEIGHT};
 		DrawPoint(posOnScreen, ColorGroupColors[particles[i].colorGroup]);
+		// FrameBufferSetPixV(posOnScreen, ColorGroupColors[particles[i].colorGroup]);
 	}
 }
